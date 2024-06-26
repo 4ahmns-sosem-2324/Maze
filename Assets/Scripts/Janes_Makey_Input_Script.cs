c643b70eb0ef34fcfe3419f95f5f24fd82b3dfd5
JansenGrizzly <128624132+JansenGrizzly@users.noreply.github.com>
2024-06-26 08:35:46 +0200

    -

--- Assets/Scripts/Janes_Makey_Input_Script.cs
+++ Assets/Scripts/Janes_Makey_Input_Script.cs
@@ -14,12 +14,15 @@ public class Janes_Makey_Input_Script : MonoBehaviour
     public bool code6 = false;
     public bool code7 = false;
 
+    public bool firstDigitis1 = false;
+    public bool secondDigitis1 = false;
+    public bool thirdDigitis1 = false;
 
     /*  
         000     0
         100     1
         110     2
-        111     3
+        111     3           Kombinationsmöglichkeintenübersicht!
         101     4
         001     5
         010     6
@@ -32,20 +35,75 @@ public class Janes_Makey_Input_Script : MonoBehaviour
         
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            
-            Debug.Log("Links");
+            firstDigitis1 = true;
+            //Debug.Log("Links/Up");
         }   
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            
-            Debug.Log("Mitte");
+            secondDigitis1 = true;
+            //Debug.Log("Mitte/Down");
         }   
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            
-            Debug.Log("Rechts");   
+            thirdDigitis1 = true;
+            //Debug.Log("Rechts/Space");   
         }
-    }
+
+
+
+        //Hier beginnt die Abfrage und zuweisung der Kombinationen
+
+
+
+        if (firstDigitis1 == true && secondDigitis1 == false && thirdDigitis1 == false)
+        {
+            Debug.Log("#1");
+        }
+
+
+        if (firstDigitis1 == true && secondDigitis1 == true && thirdDigitis1 == false)
+        {
+            Debug.Log("#2");
+        }
+
+
+        if (firstDigitis1 == true && secondDigitis1 == true && thirdDigitis1 == true)
+        {
+            Debug.Log("#3");
+        }
+
+
+        if (firstDigitis1 == true && secondDigitis1 == false && thirdDigitis1 == true)
+        {
+            Debug.Log("#4");
+        }
+
+
+        if (firstDigitis1 == false && secondDigitis1 == false && thirdDigitis1 == true)
+        {
+            Debug.Log("#5");
+        }
+
+
+        if (firstDigitis1 == false && secondDigitis1 == true && thirdDigitis1 == false)
+        {
+            Debug.Log("#6");
+        }
+
+
+        if (firstDigitis1 == false && secondDigitis1 == true && thirdDigitis1 == true)
+        {
+            Debug.Log("#7");
+        }
+
+
+
+        // Zum Schluss den Input wieder auf "000" setzen
+
+        firstDigitis1 = false;
+        secondDigitis1 = false;
+        thirdDigitis1 = false;
+}
 }
